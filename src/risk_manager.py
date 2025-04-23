13f2a54ead7901e10de3504447a2edb2075d9d60
PABLO ANDRES RAMIREZ MENESES <64621531+Pablo-app-developer@users.noreply.github.com>
2025-04-22 22:44:26 -0500

    Agregando módulo de gestión de riesgos

---
+++ src/risk_manager.py
@@ -0,0 +1,11 @@
+"""
+Gestión de riesgos y tamaño de posiciones
+"""
+
+class RiskManager:
+    def __init__(self):
+        pass
+
+    def calculate_position_size(self, signal):
+        """Calcula el tamaño óptimo de la posición"""
+        pass
