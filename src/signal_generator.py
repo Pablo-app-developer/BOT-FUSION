781eeb8d1dbcafdc7ad71781edf67be007cf8b3e
PABLO ANDRES RAMIREZ MENESES <64621531+Pablo-app-developer@users.noreply.github.com>
2025-04-22 22:44:15 -0500

    Agregando módulo de generador de señales

---
+++ src/signal_generator.py
@@ -0,0 +1,11 @@
+"""
+Generador de señales de trading basado en estrategias SMC
+"""
+
+class SignalGenerator:
+    def __init__(self):
+        pass
+
+    def generate_signals(self, analysis_data):
+        """Genera señales de trading"""
+        pass
