752294810e7e89ee10ffdbd14793f830ebe38d8c
PABLO ANDRES RAMIREZ MENESES <64621531+Pablo-app-developer@users.noreply.github.com>
2025-04-22 22:43:00 -0500

    Agregando módulo de análisis técnico

---
+++ src/technical_analysis.py
@@ -0,0 +1,11 @@
+"""
+Módulo para análisis técnico y procesamiento de datos
+"""
+
+class TechnicalAnalysis:
+    def __init__(self):
+        pass
+
+    def analyze(self, data):
+        """Realiza análisis técnico sobre los datos"""
+        pass
