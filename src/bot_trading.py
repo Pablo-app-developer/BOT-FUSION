ba0f630b1f714a8b886134c9a03132ff35616cc9
PABLO ANDRES RAMIREZ MENESES <64621531+Pablo-app-developer@users.noreply.github.com>
2025-04-22 22:42:32 -0500

    Agregando clase principal del bot de trading

---
+++ src/bot_trading.py
@@ -0,0 +1,11 @@
+"""
+Clase principal del bot de trading que integra todos los componentes
+"""
+
+class BotTrading:
+    def __init__(self):
+        pass
+
+    def run(self):
+        """Ejecuta el ciclo principal del bot"""
+        pass
